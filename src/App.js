e24502d0a90285328ce8e0b6b9238c6c50c5618b
Shishir Tiwari <shishir456tiwari@gmail.com>
2021-12-22 11:17:24 +0530

    pacakge.json homepage linked fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/symbl-ai-iee">
       <Routes>
         <Route path="/" exact element={<Home />} />
       </Routes>
